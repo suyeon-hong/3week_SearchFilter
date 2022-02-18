db0cb63209fa64464fb3bd42a12e94930e3387b9
Jeongs <kimm.jeongs@gmail.com>
2022-02-18 10:32:10 +0900

    :sparkles:  Feat: 첫번째, 마지막 페이지 이동 로직 구현

--- src/components/domain/Pagination/Pagination.tsx
+++ src/components/domain/Pagination/Pagination.tsx
@@ -24,73 +24,91 @@ const Pagination = ({
   // @NOTE: currentPage가 totalPost를 넘지 않으면 groupPages 부터 10개씩 보여준다.
   // @NOTE: currentPage가 totalPost를 넘으면 groupPages부터 postPerPage까지 더해서 보여준다.
   const MAX_PAGES_PER_GROUP = 10;
-  const lastIndex = groupPages * MAX_PAGES_PER_GROUP + MAX_PAGES_PER_GROUP;
-  // @NOTE: groupPages가 0부터 시작하므로 10을 곱하고 10을 더해줘야 함.
-  const firstIndex = lastIndex - 10;
+
+  const firstIndex = (groupPages - 1) * MAX_PAGES_PER_GROUP + 1;
+  const lastIndex = firstIndex + MAX_PAGES_PER_GROUP - 1;
   const [indexListPerGroup, setIndexListPerGroup] = useState<number[]>([]);
 
   useEffect(() => {
     const list = [];
 
     for (let i = firstIndex; i <= lastIndex; i++) {
-      if (i === 0) continue;
       if (i > totalPages) break;
       list.push(i);
     }
 
     setIndexListPerGroup([...list]);
-  }, [groupPages]);
+  }, [totalPages, groupPages]);
 
-  const handlePrevNextBtn = (direction: Direction) => {
+  const handleSingleBtn = (direction: Direction) => {
     setGroupPages((prevGroup) =>
       direction === 'next' ? prevGroup + 1 : prevGroup - 1
     );
+    setCurrentIndex((prevIndex) =>
+      direction === 'next'
+        ? prevIndex + MAX_PAGES_PER_GROUP
+        : prevIndex - MAX_PAGES_PER_GROUP
+    );
+  };
+
+  const handleDoubleBtn = (direction: Direction) => {
+    // @NOTE: 첫번째 / 마지막 그룹 페이지로 이동
+    // setGroupPages((prevGroup) =>
+    // Math.ceil(totalPages / MAX_PAGES_PER_GROUP), groupPages
+    switch (direction) {
+      case 'next': {
+        const lastGroupPages = Math.ceil(totalPages / MAX_PAGES_PER_GROUP);
+        setGroupPages(lastGroupPages);
+        setCurrentIndex((lastGroupPages - 1) * MAX_PAGES_PER_GROUP + 1);
+        break;
+      }
+      case 'prev':
+        setGroupPages(1);
+        setCurrentIndex(1);
+        break;
+      default:
+        console.log('hi');
+    }
   };
 
   const handleIndexBtn = (currentIdx: number) => {
     setCurrentIndex(currentIdx);
   };
 
-  console.log(currentIndex, 'cur');
-
   return (
     <S.PaginationWrapper>
       <PaginationBox
         mode="double"
         direction="prev"
-        checkEndIndex={firstIndex <= 0}
-        onClick={() => handlePrevNextBtn('prev')}
+        checkEndIndex={firstIndex <= 1}
+        onClick={() => handleDoubleBtn('prev')}
       />
       <PaginationBox
         mode="single"
         direction="prev"
-        checkEndIndex={firstIndex <= 0}
-        onClick={() => handlePrevNextBtn('prev')}
+        checkEndIndex={firstIndex <= 1}
+        onClick={() => handleSingleBtn('prev')}
       />
       {React.Children.toArray(
-        indexListPerGroup.map((pageIndex) => {
-          console.log(pageIndex, currentIndex);
-
-          return (
-            <PaginationBox
-              mode="none"
-              content={pageIndex}
-              aria-current={currentIndex === pageIndex ? 'page' : null}
-              onClick={() => handleIndexBtn(pageIndex)}
-            />
-          );
-        })
+        indexListPerGroup.map((pageIndex) => (
+          <PaginationBox
+            mode="none"
+            content={pageIndex}
+            aria-current={currentIndex === pageIndex ? 'page' : null}
+            onClick={() => handleIndexBtn(pageIndex)}
+          />
+        ))
       )}
       <PaginationBox
         mode="single"
         direction="next"
-        onClick={() => handlePrevNextBtn('next')}
+        onClick={() => handleSingleBtn('next')}
         checkEndIndex={lastIndex >= totalPages}
       />
       <PaginationBox
         mode="double"
         direction="next"
-        onClick={() => handlePrevNextBtn('next')}
+        onClick={() => handleDoubleBtn('next')}
         checkEndIndex={lastIndex >= totalPages}
       />
     </S.PaginationWrapper>
