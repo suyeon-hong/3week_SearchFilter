a55c9e2a94fd59716025774dae1984f32d4f94b0
kokoball <kbp4154@naver.com>
2022-02-18 21:14:46 +0900

    :sparkles:  Feat: 검색 기능 추가
    Closes #10

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,24 +1,64 @@
 import { Input } from '@components/base';
 import { PaginationWrapper, TagList, MainBox } from '@components/domain';
+import { EmptyBox } from '@components/base';
+import { matchName } from '@utils/functions';
 import { IMockData } from '@types';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './Style';
 
 interface MainProps {
   data?: IMockData[];
 }
 
-const Main = ({ data }: MainProps) => {
-  const [checkedBrand, setCheckedBrand] = useState<string>('');
-  const [userInput, setUserInput] = useState<string>('');
+const data = [
+  { 제품명: '6년근고려홍삼정스틱', 브랜드: '배달의민족' },
+  { 제품명: '마그네슘정', 브랜드: '요기요' },
+  { 제품명: '비타민D 1000IU', 브랜드: '뱅크샐러드' },
+  { 제품명: '비타민D 1000IU VitaminD 1000IU', 브랜드: '위코드' },
+  { 제품명: '비타민D 1000IU 츄어블', 브랜드: '배달의민족' },
+  { 제품명: '비타민D 1000Q연질캡슐', 브랜드: 'SKT' },
+  { 제품명: '비타민D 1080 IU', 브랜드: '카카오페이' },
+  { 제품명: '비타민D 1099IU', 브랜드: '위코드' },
+  { 제품명: '비타민D 2000', 브랜드: '카카오톡' },
+  { 제품명: '비타민D 2000 I.U', 브랜드: '배달의민족' },
+  { 제품명: '비타민D 2000 IU', 브랜드: '다이소' },
+];
+export interface updateFieldProps {
+  field: string;
+  value: string;
+  update: boolean;
+}
+
+export interface stateProps {
+  results: resultsProps[];
+}
+export interface resultsProps {
+  제품명: string | null;
+  브랜드: string;
+}
+
+const Main = () => {
+  const [checkedBrand, setCheckedBrand] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [results, setResult] = useState<stateProps | never[] | any>([]);
 
   console.log('checkedBrand: ', checkedBrand, 'userInput: ', userInput);
+
+  useEffect(() => {
+    const results = data.filter(
+      (item) => true === matchName(item.제품명, userInput)
+    );
+    setResult({ results });
+  }, [userInput]);
+
+  console.log(results.results, '결과');
 
   return (
     <S.PageWrapper>
       <Input setUserInput={setUserInput} />
+
       <TagList supplementInfo={data} setCheckedBrand={setCheckedBrand} />
-      <MainBox items={data} />
+      <MainBox items={results.results} />
       <PaginationWrapper list={data} />
     </S.PageWrapper>
   );
---
+++ src/utils/functions.ts
@@ -0,0 +1,7 @@
+// 검색해야할 문자열을 키워드와 비교하여 매칭이 되는지 체크
+export const matchName = (name: string, keyword: string): boolean => {
+  const keyLen = keyword.length;
+  name = name.toLowerCase().substring(0, keyLen);
+  if (keyword === '') return false;
+  return name === keyword.toString().toLowerCase();
+};
