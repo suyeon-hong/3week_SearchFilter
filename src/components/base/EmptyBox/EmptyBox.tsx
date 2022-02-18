79bc12f209853a78328b45b699a19f5fe9846947
kokoball <kbp4154@naver.com>
2022-02-18 12:01:32 +0900

    :sparkles:  Feat: EmptyBox 추가
    Closes #2

---
+++ src/components/base/EmptyBox/EmptyBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Style';
+
+const EmptyBox = () => {
+  return (
+    <>
+      <S.Content>검색 결과가 없습니다😥</S.Content>
+    </>
+  );
+};
+
+export default EmptyBox;
