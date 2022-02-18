02132573fadf0a7a3c6097437eb3eb1b333a7d02
sueyon-hong <bellahong95@gmail.com>
2022-02-18 14:28:53 +0900

    :sparkles:  Feat: Input 컴포넌트 구현
    Closes #1

--- src/components/base/Input/Input.tsx
+++ src/components/base/Input/Input.tsx
@@ -1,10 +1,13 @@
 import * as S from './Style';
+import { RiSearchLine } from 'react-icons/ri';
 
 const Input = () => {
   return (
     <S.Form>
-      <S.UserInput />
-      <S.SearchBtn></S.SearchBtn>
+      <S.UserInput placeholder="제품명, 브랜드명 검색" />
+      <S.SearchBtn>
+        <RiSearchLine />
+      </S.SearchBtn>
     </S.Form>
   );
 };
--- src/components/base/Input/Style.ts
+++ src/components/base/Input/Style.ts
@@ -1,14 +1,14 @@
 import { flexbox } from '@styles/commonStyles';
 import { font18 } from '@styles/fonts';
 import styled from 'styled-components';
+import { removeTextPointer } from '@styles/commonStyles';
 
 export const Form = styled.form`
   max-width: 477px;
   height: 60px;
   margin: 0 auto;
   position: relative;
-  padding-left: ${({ theme }) => `${theme.gaps.base}px`};
-  padding-right: ${({ theme }) => `${theme.gaps.base * 6}px`};
+  ${removeTextPointer};
 `;
 
 export const UserInput = styled.input`
@@ -16,10 +16,17 @@ export const UserInput = styled.input`
   height: 100%;
   border: none;
   background: #fff;
-  ${font18(500)}
+  ${font18(400)};
+  border-radius: ${({ theme }) => `${theme.borderRadius.small}px`};
+  padding-left: ${({ theme }) => `${theme.gaps.base * 2}px`};
+  padding-right: ${({ theme }) => `${theme.gaps.base * 6}px`};
 
   &:focus {
     outline: none;
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray};
   }
 `;
 
@@ -33,4 +40,6 @@ export const SearchBtn = styled.button`
   top: 50%;
   right: ${({ theme }) => `${theme.gaps.base}px`};
   transform: translateY(-50%);
+  font-size: 24px;
+  color: #fff;
 `;
