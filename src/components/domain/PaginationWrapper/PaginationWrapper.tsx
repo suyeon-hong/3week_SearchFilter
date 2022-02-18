3688ae8cf290d0ce5510a34a220fe046fe18ca34
Jeongs <kimm.jeongs@gmail.com>
2022-02-18 10:32:30 +0900

    :sparkles:  Feat: 한번에 보여줘야 할 페이지 로직 구현

--- src/components/domain/PaginationWrapper/PaginationWrapper.tsx
+++ src/components/domain/PaginationWrapper/PaginationWrapper.tsx
@@ -1,21 +1,31 @@
 import { Pagination } from '@components/domain';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { IMockData } from '@types';
 
 interface PaginationWrapper {
-  listLength: number;
+  list: IMockData[];
 }
 
-const PaginationWrapper = ({ listLength }: PaginationWrapper) => {
+const PaginationWrapper = ({ list }: PaginationWrapper) => {
   const [currentIndex, setCurrentIndex] = useState(1); // @NOTE: 누를 때마다 변화하는 값
   const [postPerPage, setPostPerPage] = useState(6); // @NOTE: 한 페이지당 보여줄 포스트 수
   const [groupPages, setGroupPages] = useState(
-    Math.floor(currentIndex / postPerPage)
+    Math.floor(currentIndex / postPerPage) + 1
   );
+  const offset = (currentIndex - 1) * postPerPage;
 
   return (
     <>
+      {React.Children.toArray(
+        list.slice(offset, offset + postPerPage).map(({ 제품명, 브랜드 }) => (
+          <li>
+            {제품명} {브랜드}
+          </li>
+        ))
+      )}
       <Pagination
-        totalPages={listLength}
+        totalPages={list.length}
+        // totalPages={37}
         groupPages={groupPages}
         postPerPage={postPerPage}
         currentIndex={currentIndex}
