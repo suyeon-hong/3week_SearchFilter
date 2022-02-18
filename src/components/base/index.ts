89a00237388a930ad89fff8553e2d0a0765c8a50
sueyon-hong <bellahong95@gmail.com>
2022-02-18 17:21:49 +0900

    :fire:  Prune: MainBox 폴더위치 수정

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,7 +1,6 @@
 export { default as PaginationBox } from './PaginationBox/PaginationBox';
 export { default as Header } from './Header/Header';
 export { default as ItemBox } from './ItemBox/ItemBox';
-export { default as MainBox } from './MainBox/MainBox';
 export { default as EmptyBox } from './EmptyBox/EmptyBox';
 export { default as Tag } from './Tag/Tag';
 export { default as Input } from './Input/Input';
